6c29f97a584766cec37818f2f1cc377d38abad79
YoussefMaia <37788119+YoussefMaia@users.noreply.github.com>
2021-12-30 23:55:30 +0100

    Add CORS

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app  = express()
+const cors   = require('cors')
 
 // Get routes
 const user = require('./api/user')
 const category = require('./api/category')
 
+
+app.use(cors({
+    origin: [
+        "http://localhost:3000", 
+        "http://localhost:3001", 
+        "http://localhost:3003"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+}))
 
 const PORT = process.env.PORT || 5050;
 
